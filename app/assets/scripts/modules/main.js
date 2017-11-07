95ebcec5b0ea6d5bb5c21884f4837defaf62d68a
Richard Chang <richardlwchang@gmail.com>
2017-11-07 10:48:48 -0800

    Complete scroll reveal effects

--- app/assets/scripts/modules/main.js
+++ app/assets/scripts/modules/main.js
@@ -1,4 +1,7 @@
 const $ = require('jquery');
 import Navbar from "./Navbar";
+import RevealOnScroll from "./RevealOnScroll";
 
 var navbar = new Navbar();
+var revealFeatureItem = new RevealOnScroll(".feature-item", "85%");
+var revealTestimonial = new RevealOnScroll(".testimonial", "60%");
